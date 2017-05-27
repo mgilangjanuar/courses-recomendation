2d5a2f376a57ff5bb5a1e262ca7cff2fe521d613
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2017-05-27 20:32:07 +0700

    remove db.php in config

--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=matkul-rec',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
